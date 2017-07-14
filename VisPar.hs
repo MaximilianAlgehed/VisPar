e91ad2c727e5efbb4bb7320bc897720ab6d1a521
Maximilian Algehed <m.algehed@gmail.com>
2017-07-14 16:35:24 +0200

    Started working towards a sequential scheduler whose only job is to produce the graph

--- VisPar.hs
+++ VisPar.hs
@@ -20,7 +20,8 @@ module VisPar (
    spawn, spawnNamed
  ) where
 
-
+import Control.Monad.State as S hiding (put)
+import qualified Control.Monad.State as S
 import Control.Monad as M hiding (mapM, sequence, join)
 import Prelude hiding (mapM, sequence, head,tail)
 import Data.IORef
@@ -36,7 +37,7 @@ import Data.List
 
 makeGraph :: Bool -> Maybe String -> Par a -> Graph
 makeGraph b s = normalise . snd . unsafePerformIO .
-  runPar_internal b (maybe "0" id s) True
+  runPar_internal b (maybe "0" id s)
 
 normalise :: Graph -> Graph
 normalise g = let labels  = zip (sort . nub $ tag . snd <$> labNodes g) [0..]
@@ -88,6 +89,8 @@ instance Labellable Name where
 
 -- ---------------------------------------------------------------------------
 
+type Run a = StateT Sched IO a
+
 data Trace = forall a . Get (IVar a) (a -> Trace)
            | forall a . Put (IVar a) a Trace
            | forall a . New (IVarContents a) (IVar a -> Trace)
@@ -95,185 +98,128 @@ data Trace = forall a . Get (IVar a) (a -> Trace)
            | SetName String Trace
            | GetName (String -> Trace)
            | Done
-           | forall a . LiftIO (IO a) (a -> Trace)
 
-makeC :: Sched -> Name -> IO Name
-makeC queue thisThread
-  | makeCs queue = do
-    atomicModifyIORef (graph queue) $
-        \g -> let (n:_) = newNodes 1 g
-                  newName = Name n (tag thisThread) (altName thisThread) "start"
-              in (insEdge (nid thisThread, n, C)
-                  (insNode (n, newName) g)
-                 , newName)
-  | otherwise = return thisThread
+makeC :: Name -> Run Name
+makeC thisThread = do
+  cond <- gets makeCs
+  if cond then do
+    queue <- S.get
+    let g       = graph queue
+        (n:_)   = newNodes 1 g
+        newName = Name n (tag thisThread) (altName thisThread) "start"
+    S.put $ queue { graph = insEdge (nid thisThread, n, C)
+                                    (insNode (n, newName) g) }
+    return newName
+  else
+    return thisThread
 
-setEvent :: Sched -> Name -> Event -> IO ()
-setEvent queue thisThread event = do
-  atomicModifyIORef (graph queue) $
-    \g -> (nmap (\nm@(Name n t m _) ->
-      if n == nid thisThread then
-        Name n t m event
-      else
-        nm) g, ())
+setEvent :: Name -> Event -> Run ()
+setEvent thisThread event = do
+  modify $ \queue ->
+    let g = graph queue in
+      queue
+        { graph = nmap (\nm@(Name n t m _) ->
+                         if n == nid thisThread then
+                           Name n t m event
+                         else nm
+                       ) g
+       }
 
 -- | The main scheduler loop.
-sched :: Bool -> Sched -> (Trace, Name) -> IO ()
-sched _doSync queue (t, n) = loop t n
- where
-  loop :: Trace -> Name -> IO ()
-  loop t thisThread = case t of
+sched :: (Trace, Name) -> Run ()
+sched (t, n) = loop t n
+  where
+   loop :: Trace -> Name -> Run ()
+   loop t thisThread = case t of
 
-    SetName s t -> do
-      newName <- atomicModifyIORef (graph queue) $
-        \g -> (nmap (\nm ->
-          if nid nm == nid thisThread then
-            nm { altName = Just s}
-          else
-            nm) g, thisThread { altName = Just s})
-      loop t newName
+     SetName s t -> do
+       newName <- atomicModifyIORef (graph queue) $
+         \g -> (nmap (\nm ->
+           if nid nm == nid thisThread then
+             nm { altName = Just s}
+           else
+             nm) g, thisThread { altName = Just s})
+       loop t newName
 
-    GetName c -> loop
-      (c (maybe (show (tag thisThread)) id $ altName thisThread))
-      thisThread
+     GetName c -> loop
+       (c (maybe (show (tag thisThread)) id $ altName thisThread))
+       thisThread
 
-    New a f -> do
-      r <- newIORef (a, thisThread)
-      newName <- makeC queue thisThread
-      setEvent queue thisThread "new"
-      loop (f (IVar r)) newName
+     New a f -> do
+       r <- newIORef (a, thisThread)
+       newName <- makeC thisThread
+       setEvent thisThread "new"
+       loop (f (IVar r)) newName
 
-    Get (IVar v) c -> do
-      setEvent queue thisThread "get"
-      (e, source) <- readIORef v
-      newName <- makeC queue thisThread 
-      let c' src a = (do
-                        atomicModifyIORef (graph queue) $
-                          \g -> (insEdge (nid src, nid thisThread, G) g, ())
-                     , c a)
-      case e of
-         Full a -> do
-                    fst (c' source a)
-                    loop (snd (c' source a)) newName
-         _other -> do
-           r <- atomicModifyIORef v $ \e -> case e of
-                        (Empty, src)      -> ((Blocked [(c', newName)], src),     reschedule queue)
-                        (Full a, src)     -> ((Full a, src),  do
-                                                                fst (c' src a)
-                                                                loop (snd (c' src a)) newName)
-                        (Blocked cs, src) ->
-                          ((Blocked ((c', newName):cs), src), reschedule queue)
-           r
+     Get (IVar v) c -> do
+       setEvent thisThread "get"
+       (e, source) <- readIORef v
+       newName <- makeC thisThread 
+       let c' src a = (do
+                         atomicModifyIORef (graph queue) $
+                           \g -> (insEdge (nid src, nid thisThread, G) g, ())
+                      , c a)
+       case e of
+          Full a -> do
+                     fst (c' source a)
+                     loop (snd (c' source a)) newName
+          _other -> do
+            r <- atomicModifyIORef v $ \e -> case e of
+                         (Empty, src)      -> ((Blocked [(c', newName)], src),     reschedule queue)
+                         (Full a, src)     -> ((Full a, src),  do
+                                                                 fst (c' src a)
+                                                                 loop (snd (c' src a)) newName)
+                         (Blocked cs, src) ->
+                           ((Blocked ((c', newName):cs), src), reschedule queue)
+            r
 
-    Put (IVar v) a t  -> do
-      cs <- atomicModifyIORef v $ \(e, _) -> case e of
-               Empty      -> ((Full a, thisThread), [])
-               Full _     -> error "multiple put"
-               Blocked cs -> ((Full a, thisThread), cs)
-      mapM_ (\(c, n) -> do fst (c thisThread a);
-                           pushWork queue (snd (c thisThread a)) n) cs
-      newName <- makeC queue thisThread
-      setEvent queue thisThread "put"
-      loop t newName 
+     Put (IVar v) a t  -> do
+       cs <- atomicModifyIORef v $ \(e, _) -> case e of
+                Empty      -> ((Full a, thisThread), [])
+                Full _     -> error "multiple put"
+                Blocked cs -> ((Full a, thisThread), cs)
+       mapM_ (\(c, n) -> do fst (c thisThread a);
+                            pushWork (snd (c thisThread a)) n) cs
+       newName <- makeC thisThread
+       setEvent thisThread "put"
+       loop t newName 
 
-    Fork child parent -> do
-         setEvent queue thisThread "fork"
-         newName <-  atomicModifyIORef (graph queue) $
-            \g -> let (newNode:_) = newNodes 1 g
-                  in (insEdge (nid thisThread, newNode, F)
-                      $ insNode (newNode, (Name newNode newNode Nothing "start")) g
-                     , Name newNode newNode Nothing "start")
-         pushWork queue child newName
-         newName' <- makeC queue thisThread
-         loop parent newName'
+     Fork child parent -> do
+       setEvent thisThread "fork"
+       newName <-  atomicModifyIORef (graph queue) $
+          \g -> let (newNode:_) = newNodes 1 g
+                in (insEdge (nid thisThread, newNode, F)
+                    $ insNode (newNode, (Name newNode newNode Nothing "start")) g
+                   , Name newNode newNode Nothing "start")
+       pushWork child newName
+       newName' <- makeC thisThread
+       loop parent newName'
 
-    Done -> do
-         setEvent queue thisThread "done"
-         if _doSync
-         then reschedule queue
--- We could fork an extra thread here to keep numCapabilities workers
--- even when the main thread returns to the runPar caller...
-         else do putStrLn " [par] Forking replacement thread..\n"
-                 forkIO (reschedule queue); return ()
--- But even if we don't we are not orphaning any work in this
--- threads work-queue because it can be stolen by other threads.
---       else return ()
+     Done -> setEvent thisThread "done"
 
-    LiftIO io c -> do
-        r <- io
-        loop (c r) thisThread
+pushWork :: Trace -> Name -> Run ()
+pushWork t threadName = do
+  modify $ \queue ->
+    let ts = workpool queue in
+    queue { workpool = (t, threadName):ts }
 
 -- | Process the next item on the work queue or, failing that, go into
 --   work-stealing mode.
-reschedule :: Sched -> IO ()
-reschedule queue@Sched{ workpool } = do
-  e <- atomicModifyIORef workpool $ \ts ->
-         case ts of
-           []      -> ([], Nothing)
-           (t:ts') -> (ts', Just t)
-  case e of
-    Nothing -> steal queue
-    Just t  -> sched True queue t
-
-
--- RRN: Note -- NOT doing random work stealing breaks the traditional
--- Cilk time/space bounds if one is running strictly nested (series
--- parallel) programs.
-
--- | Attempt to steal work or, failing that, give up and go idle.
-steal :: Sched -> IO ()
-steal q@Sched{ idle, scheds, no=my_no } = do
-  -- printf "cpu %d stealing\n" my_no
-  go scheds
-  where
-    go [] = do m <- newEmptyMVar
-               r <- atomicModifyIORef idle $ \is -> (m:is, is)
-               if length r == numCapabilities - 1
-                  then do
-                     -- printf "cpu %d initiating shutdown\n" my_no
-                     mapM_ (\m -> putMVar m True) r
-                  else do
-                    done <- takeMVar m
-                    if done
-                       then do
-                         -- printf "cpu %d shutting down\n" my_no
-                         return ()
-                       else do
-                         -- printf "cpu %d woken up\n" my_no
-                         go scheds
-    go (x:xs)
-      | no x == my_no = go xs
-      | otherwise     = do
-         r <- atomicModifyIORef (workpool x) $ \ ts ->
-                 case ts of
-                    []     -> ([], Nothing)
-                    (x:xs) -> (xs, Just x)
-         case r of
-           Just t  -> do
-              -- printf "cpu %d got work from cpu %d\n" my_no (no x)
-              sched True q t
-           Nothing -> go xs
-
--- | If any worker is idle, wake one up and give it work to do.
-pushWork :: Sched -> Trace -> Name -> IO ()
-pushWork Sched { workpool, idle } t threadName = do
-  atomicModifyIORef workpool $ \ts -> ((t, threadName):ts, ())
-  idles <- readIORef idle
-  when (not (null idles)) $ do
-    r <- atomicModifyIORef idle (\is -> case is of
-                                          [] -> ([], return ())
-                                          (i:is) -> (is, putMVar i False))
-    r -- wake one up
+reschedule :: Run () 
+reschedule = do
+  workpool <- gets workpool
+  case workpool of
+    []     -> do
+      liftIO $ putStrLn "Dependency failure, empty workpool"
+      return ()
+    (t:ts) -> do
+      modify $ \queue -> queue { workpool = ts }
+      sched t
 
 data Sched = Sched
-    { no       :: {-# UNPACK #-} !Int,
-      graph    :: IORef Graph,
+    { graph    :: Graph,
       makeCs   :: Bool,
-      workpool :: IORef [(Trace, Name)],
-      idle     :: IORef [MVar Bool],
-      scheds   :: [Sched] -- Global list of all per-thread workers.
-    }
---  deriving Show
+      workpool :: [(Trace, Name)] }
 
 newtype Par a = Par {
     runCont :: (a -> Trace) -> Trace
@@ -316,54 +262,20 @@ data IVarContents a = Full a
                     | Blocked [(Name -> a -> (IO (), Trace), Name)]
 
 {-# INLINE runPar_internal #-}
-runPar_internal :: Bool -> String -> Bool -> Par a -> IO (a, Graph)
-runPar_internal b s _doSync x = do
-   workpools <- replicateM numCapabilities $ newIORef []
-   idle <- newIORef []
-   graph <- newIORef (insNode (0, Name 0 0 (Just s) "start") Data.Graph.Inductive.empty)
-   let states = [ Sched { no=x
-                        , workpool=wp
-                        , makeCs = b
-                        , graph=graph
-                        , idle
-                        , scheds=states }
-                | (x,wp) <- zip [0..] workpools ]
+runPar_internal :: Bool -> String -> Par a -> IO (a, Graph)
+runPar_internal makeCs s x = do
+   let gr = insNode (0, Name 0 0 (Just s) "start") Data.Graph.Inductive.empty
+       queue = Sched gr makeCs []
+   rref <- newIORef (Empty, Name 0 0 (Just s) "")
+   s <- flip execStateT queue $
+    sched (runCont (x >>= put_ (IVar rref)) (const Done), Name 0 0 (Just s) "")
 
-#if __GLASGOW_HASKELL__ >= 701 /* 20110301 */
-    --
-    -- We create a thread on each CPU with forkOn.  The CPU on which
-    -- the current thread is running will host the main thread; the
-    -- other CPUs will host worker threads.
-    --
-    -- Note: GHC 7.1.20110301 is required for this to work, because that
-    -- is when threadCapability was added.
-    --
-   (main_cpu, _) <- threadCapability =<< myThreadId
-#else
-    --
-    -- Lacking threadCapability, we always pick CPU #0 to run the main
-    -- thread.  If the current thread is not running on CPU #0, this
-    -- will require some data to be shipped over the memory bus, and
-    -- hence will be slightly slower than the version above.
-    --
-   let main_cpu = 0
-#endif
+   let g = graph s
 
-   m <- newEmptyMVar
-   forM_ (zip [0..] states) $ \(cpu,state) ->
-        forkOn cpu $
-          if (cpu /= main_cpu)
-             then reschedule state
-             else do
-                  rref <- newIORef (Empty, Name 0 0 (Just s) "")
-                  sched _doSync state ((runCont (x >>= put_ (IVar rref)) (const Done)), (Name 0 0 (Just s) ""))
-                  readIORef rref >>= putMVar m
-
-   r <- takeMVar m
-   g <- readIORef graph
+   r <- readIORef rref
    case r of
      (Full a, _) ->
-      return (a, nmap (\lab -> if b then lab else lab {event = ""}) g)
+      return (a, nmap (\lab -> if makeCs then lab else lab {event = ""}) g)
      _ -> error "no result"
 
 
@@ -374,21 +286,7 @@ runPar_internal b s _doSync x = do
 --   `runST` or with newer libraries that export a Par monad, such as
 --   `lvish`.
 runPar :: Par a -> a
-runPar = fst . unsafePerformIO . runPar_internal False "" True
-
--- | A version that avoids an internal `unsafePerformIO` for calling
---   contexts that are already in the `IO` monad.
---
---   Returning any value containing IVar is still disallowed, as it
---   can compromise type safety.
-runParIO :: Par a -> IO a
-runParIO = (fmap fst) . runPar_internal False "" True
-
--- | An asynchronous version in which the main thread of control in a
--- Par computation can return while forked computations still run in
--- the background.
-runParAsync :: Par a -> a
-runParAsync = fst . unsafePerformIO . runPar_internal False "" False
+runPar = fst . unsafePerformIO . runPar_internal True ""
 
 -- -----------------------------------------------------------------------------
 
